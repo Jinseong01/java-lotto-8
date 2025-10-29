e1057a80b11b3d3cd729761fa308561c1d15e16a
Jinseong <jinsung528@gmail.com>
2025-10-29 17:35:43 +0900

    test(PurchasePriceValidatorTest): PurchasePriceValidator 단위 테스트 작성
    - 최소/최대 이외의 값, 로또 1장 당 가격 단위 미준수 값 입력에 대해 테스트

---
+++ src/test/java/lotto/util/PurchasePriceValidatorTest.java
@@ -0,0 +1,31 @@
+package lotto.util;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import lotto.enums.ErrorMessage;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class PurchasePriceValidatorTest {
+
+    private final PurchasePriceValidator purchasePriceValidator = new PurchasePriceValidator();
+
+    @ParameterizedTest
+    @DisplayName("최소/최대 범위 이외의 값 입력 시, 예외 발생")
+    @ValueSource(ints = {4000, 120_000})
+    public void testValidateRange(int input) {
+        assertThatThrownBy(() -> purchasePriceValidator.validate(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.getMessage(ErrorMessage.ERROR_PURCHASE_PRICE_RANGE));
+    }
+
+    @ParameterizedTest
+    @DisplayName("로또 가격 단위 미준수 금액 입력 시, 예외 발생")
+    @ValueSource(ints = {5_500, 12_345})
+    public void testValidateUnit(int input) {
+        assertThatThrownBy(() -> purchasePriceValidator.validate(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ErrorMessage.getMessage(ErrorMessage.ERROR_PURCHASE_PRICE_UNIT));
+    }
+}
