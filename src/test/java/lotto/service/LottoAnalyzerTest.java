4d5e16b1aa70517186ac47aa948928577881f90b
Jinseong <jinsung528@gmail.com>
2025-10-29 15:51:33 +0900

    test(LottoAnalyzerTest): LottoAnalyzer 단위 테스트 작성
    - 로또 당첨 결과 매칭, 모든 등수 결과에 포함 여부, 당첨 로또 개수 합, 수익률 계산 결과 확에 대해 테스트

---
+++ src/test/java/lotto/service/LottoAnalyzerTest.java
@@ -0,0 +1,87 @@
+package lotto.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import java.util.Map;
+import lotto.domain.Lotto;
+import lotto.domain.WinningLotto;
+import lotto.enums.LottoConfig;
+import lotto.enums.LottoRule;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class LottoAnalyzerTest {
+
+    private final LottoAnalyzer lottoAnalyzer = new LottoAnalyzer();
+    private final WinningLotto winningLotto = new WinningLotto(List.of(1, 2, 3, 4, 5, 6), 7);
+    private final List<Lotto> lottos = List.of(
+            new Lotto(List.of(1, 2, 3, 4, 5, 6)),   // 1등
+            new Lotto(List.of(1, 2, 3, 4, 5, 7)),   // 2등
+            new Lotto(List.of(1, 2, 3, 4, 5, 8)),   // 3등
+            new Lotto(List.of(1, 2, 3, 4, 8, 9)),   // 4등
+            new Lotto(List.of(1, 2, 3, 8, 9, 10)),  // 5등
+            new Lotto(List.of(1, 2, 8, 9, 10, 11))  // 꽝
+    );
+
+
+    @Test
+    @DisplayName("로또 당첨 결과 확인")
+    public void testMatch() {
+        // when
+        Map<LottoRule, Integer> result = lottoAnalyzer.match(lottos, winningLotto);
+
+        // then
+        assertThat(result.get(LottoRule.FIRST)).isEqualTo(1);
+        assertThat(result.get(LottoRule.SECOND)).isEqualTo(1);
+        assertThat(result.get(LottoRule.THIRD)).isEqualTo(1);
+        assertThat(result.get(LottoRule.FOURTH)).isEqualTo(1);
+        assertThat(result.get(LottoRule.FIFTH)).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("모든 등수가 결과에 포함되는지 확인")
+    public void testAllRankInclude() {
+        // when
+        Map<LottoRule, Integer> result = lottoAnalyzer.match(lottos, winningLotto);
+
+        // then
+        assertThat(result).containsKeys(
+                LottoRule.FIRST, LottoRule.SECOND, LottoRule.THIRD, LottoRule.FOURTH, LottoRule.FIFTH
+        );
+    }
+
+    @Test
+    @DisplayName("당첨된 로또 개수의 합 확인")
+    public void testTotalWinningCount() {
+        // when
+        Map<LottoRule, Integer> result = lottoAnalyzer.match(lottos, winningLotto);
+        int totalWinningCount = result.values().stream()
+                .mapToInt(Integer::intValue)
+                .sum();
+
+        // then
+        assertThat(totalWinningCount).isEqualTo(5);
+    }
+
+    @Test
+    @DisplayName("수익률 계산 결과 확인")
+    public void testCalculate() {
+        // given
+        int lottoCount = 7;
+        int purchasePrice = lottoCount * LottoConfig.LOTTO_PRICE.getValue();
+        Map<LottoRule, Integer> result = Map.of(
+                LottoRule.FIRST, 0,
+                LottoRule.SECOND, 0,
+                LottoRule.THIRD, 0,
+                LottoRule.FOURTH, 0,
+                LottoRule.FIFTH, 1
+        );
+
+        // when
+        double rate = lottoAnalyzer.calculateRateOfReturn(purchasePrice, result);
+
+        // then
+        assertThat(rate).isEqualTo(71.4);
+    }
+}
